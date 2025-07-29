da99ad3474e3f24087607fb2237acbd16a8e9641
Vinsmokesanjikun <shreevyas0708@gmail.com>
2025-07-30 01:51:44 +0530

    Removed uncessary files

--- init_repo.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-REPO_NAME="secure-chat-e2ee"
-gh repo create "$REPO_NAME" --public --source=. --remote=origin --push
-echo "✅ Repo created: https://github.com/$(gh repo view --json ShreeYashVyas -q .ShreeYashVyas)"
-
