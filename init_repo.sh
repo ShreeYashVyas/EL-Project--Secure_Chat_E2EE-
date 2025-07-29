e2ee907dfb52baabe394ff29b30e9aef0e6ba1d8
Shree Yash Vyas <shreevyas0708@gmail.com>
2025-07-29 17:44:38 +0530

    Update init_repo.sh

--- init_repo.sh
+++ init_repo.sh
@@ -2,4 +2,5 @@
 set -e
 REPO_NAME="secure-chat-e2ee"
 gh repo create "$REPO_NAME" --public --source=. --remote=origin --push
-echo "✅ Repo created: https://github.com/$(gh repo view --json nameWithOwner -q .nameWithOwner)"
+echo "✅ Repo created: https://github.com/$(gh repo view --json ShreeYashVyas -q .ShreeYashVyas)"
+
